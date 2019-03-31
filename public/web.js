6cf0bec1ba95e26d74d02083b7a84f38d8a5f936
Travis <Travis.Bresnahan@live.longwood.edu>
2019-03-31 19:18:37 -0400

    *Gutted old authentication / login system that interfaced with Firebase DB and started from scratch. *NOT ABLE to use built in firebase functions to streamline process, currently trying to simply use javascript push commands to throw a username, email, and real name SOMEWHERE into the table. 	^-> Current status overwrites every single time, looking into unique keys and using "update" *Favicon error in console is OK to ignore, doesn't matter for anything important other than aesthetic

--- public/web.js
+++ public/web.js
@@ -1,68 +1,9 @@
-(function() {
-	// Initialize Firebase --------------------------------------------------------
-	const config = {
-		apiKey: "AIzaSyA11FpqG-RKNpZ0loNU8HhunHUgk7EFCqY",
-		authDomain: "seniorsemgit.firebaseapp.com",
-		databaseURL: "https://seniorsemgit.firebaseio.com",
-		projectId: "seniorsemgit",
-		storageBucket: "seniorsemgit.appspot.com",
-		messagingSenderId: "348042953918"
-
-	};
-firebase.initializeApp(config);
 //-------------------------------------------------------------------------------------
-
+function saveUser() {
+	firebase.database().ref("Users/").update({
 	//Get Elements
-	const txtEmail = document.getElementById('txtEmail');
-	const txtPassword = document.getElementById('txtPassword');
-	const btnLogin = document.getElementById('btnLogin');
-	const btnSignUp = document.getElementById('btnSignUp');
-	const btnLogout = document.getElementById('btnLogout');
-
-	//Add login event
-	btnLogin.addEventListener('click', e => {
-		//Get email and pass
-		const email = txtEmail.value;
-		const pass = txtPassword.value;
-		const auth = firebase.auth();
-		//Sign in
-		const promise = auth.signInWithEmailAndPassword(email, pass);
-		promise.catch(e => console.log(e.message));
-	});
-
-
-	//Add signup event
-	btnSignUp.addEventListener('click', e => {
-		//Get email and Pass
-		// TODO: REAL EMAIL CONSTRAINTS
-		const email = txtEmail.value;
-		const pass = txtPassword.value;
-		const auth = firebase.auth();
-		//Sign up
-		const promise = auth.createUserWithEmailAndPassword(email, pass);
-		promise
-			.catch(e => console.log(e.message));
-	});
-
-	//Add logout event
-	btnLogout.addEventListener('click', e => {
-		firebase.auth().signOut();
-	});
-
-	//Add realtime listener
-	firebase.auth().onAuthStateChanged(firebaseUser => {
-		if(firebaseUser) {
-			console.log(firebaseUser);
-			//If user is logged in, show button
-			btnLogout.classList.remove('hide');
-		} else {
-			console.log('not logged in');
-			//If user is not logged in, hide button
-			btnLogout.classList.add('hide');
-		}
-	});
-
-
-}());
-//Source: https://www.youtube.com/watch?v=-OKrloDzGpU
-
+		username: document.getElementById('txtUsername').value,
+		email:	document.getElementById('txtEmail').value,
+		name:	document.getElementById('txtName').value
+})
+};

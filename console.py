385f3dce65320b547d15508499b7718bc2a02198
Anderw1234567 <adsmitty70@yahoo.com>
2019-04-01 13:34:11 -0400

    Added in our Python console program.
    Connects to firebase db
    
    pulls users and boards.
    "users" to see users.
    "boards" to see boards.
    "quit" to quit.
    
    As of now, I'm thinking that adding a "setboard" command that will take a "boardid" as a parameter to set a board as the one we are working on.

---
+++ console.py
@@ -0,0 +1,42 @@
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyA11FpqG-RKNpZ0loNU8HhunHUgk7EFCqY ",
+  "authDomain": "seniorsemgit.firebaseapp.com",
+  "databaseURL": "https://seniorsemgit.firebaseio.com",
+  "storageBucket": "seniorsemgit.appspot.com"
+}
+
+print("Connecting to database...")
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+print("Fetching users and boards...")
+
+users = db.child("Users").get()
+boards = db.child("Boards").get()
+
+print("Welcome to Super Seven KanBan! Enter 'quit' to exit.")
+
+inputString = input("> ")
+
+while(1):
+
+    if (inputString == "quit"):
+        print("Goodbye!")
+        break
+
+    if (inputString == "users"):
+        for user in users.each():
+            print(user.key(),": ", user.val())
+
+    if (inputString == "boards"):
+        for board in boards.each():
+            print(board.key(), ": ", board.val())
+
+    inputString = input("> ")
+
+
+
+print("END PROGRAM")

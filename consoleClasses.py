38b5e7b3930a3f37ff6968475f8b627e9ecd5608
Anderw1234567 <adsmitty70@yahoo.com>
2019-04-03 23:48:19 -0400

    Code that parses our DB objects into classes
     some cool list comprehension functions at the very top for looking inside our classes for testing purposes

--- consoleClasses.py
+++ consoleClasses.py
@@ -1,3 +1,10 @@
+def propKeys(cls):
+    return [i for i in cls.__dict__.keys() if i[:1] != '_']
+
+def propVals(cls):
+    return [i for i in cls.__dict__.values() if i[:1] != '_']
+
+
 class Commit:
     action = "default commit action"
     user = "default commit user"
@@ -21,8 +28,8 @@ class User:
 class Task:
     commitList = ["commitlistitem", "commitlistitem"]
     description = "default task description"
-    duedate = "default due date"
-    latestaction = "default latest action"
+    dueDate = "default due date"
+    latestAction = "default latest action"
     name = "default task name"
     ownerList = ["ownerlistitem", "ownerlistitem"]
 
@@ -30,10 +37,52 @@ class KanColumn:
     columnTitle = "default column title"
     taskList = ["tasklistitem", "tasklistitem"]
 
+
+        
+
 class Board:
     name = "default board name"
     repository = "default repository"
-    columnList = ["columnlistitem", "columnlistitem"]
-    userList = ["userlistitem", "userlistitem"]
-    
-    
+    columnList = ["columnlistitem"]
+    userList = ["userlistitem"]
+
+    def __init__(self):      
+        name = "default board name"
+        repository = "default repository"
+        columnList = ["columnlistitem"]
+        userList = ["userlistitem"]
+        
+
+  #  def __iter__(self):
+  #      for attr, value 
+
+def parseTasks(column, key, value):
+    #print("THIS COLUMN NAME: ", column.columnTitle,"KEY: ", key, " VALUE: ", value)
+    for taskKey, taskValue in value.items():
+        newTask = Task()
+        newTask.name = taskKey
+        for taskHeader, taskInfo in taskValue.items():
+            if (taskHeader == "DueDate"):
+                newTask.dueDate = taskInfo
+            if (taskHeader == "LatestAction"):
+                newTask.latestAction = taskInfo
+            if (taskHeader == "Name"):
+                newTask.name = taskInfo
+            if (taskHeader == "Description"):
+                newTask.description = taskInfo
+            column.taskList.append(newTask)
+        print(propKeys(newTask), propVals(newTask))
+
+            #print("PARSETASKS KEYY: ", taskHeader, " PARSETASKS VAL: ", taskInfo)
+
+
+def parseColumns(board, key, value):
+    #print("KEY: ", key," VALUE: " value)
+    for columnKey, columnValue in value.items():
+        newColumn = KanColumn()
+        newColumn.columnTitle = columnKey
+        parseTasks(newColumn, columnKey, columnValue)
+        board.columnList.append(newColumn)
+        print(propKeys(newColumn), propVals(newColumn))
+        #[print(t) for t in dir(newColumn) if not t.startswith('__')]
+        #print("PARSECOLUMNS KEYY: ", keyy, " PARSETASKS VAL: ", val)

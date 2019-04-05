aa67b4c35d2ee47d461ac5fbbf830c9525a436f4
isaiahf06 <isaiahf_0318@yahoo.com>
2019-04-04 21:36:06 -0400

    Fixed a few syntactical issues with the saveUser function

--- public/index.js
+++ public/index.js
@@ -2,11 +2,11 @@
 function saveUser() {
 	firebase.database().ref("Users/").push({
 	//Get Elements
-		Username: document.getElementById('txtUsername').value,
+		Username: document.getElementById('txtUsername').value
 		//Email:	document.getElementById('txtEmail').value,
 		//Name:	document.getElementById('txtName').value
 })
-}
+};
 
 
 function redirect() {

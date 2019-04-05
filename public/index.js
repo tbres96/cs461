65dbd4af8c4389b733efeddd467fe1d603a9585f
isaiahf06 <isaiahf_0318@yahoo.com>
2019-04-04 23:04:41 -0400

    Fixed issue with userSave function making the key value in data base prettier by matching it to the username value

--- public/index.js
+++ public/index.js
@@ -7,12 +7,8 @@ function saveUser() {
 	//firebase.database().ref("Users/").push({
 	//Get Elements
 	//Username: document.getElementById('txtUsername').value,
-	var dbRef = firebase.database();
-	var usersRef = dbRef.child("Users/");
-	usersRef.child(document.getElementById('txtUsername').value).set({
+	var dbRef = firebase.database().ref("Users/");
+	var usersRef = dbRef.child(document.getElementById('txtUsername').value).set({
 		Username: document.getElementById('txtUsername').value
-}, onComplete)
+})
 };
-
-
-;

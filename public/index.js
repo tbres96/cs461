62230247d89c419e847f74ac98938d6b328aef67
Travis <Travis.Bresnahan@live.longwood.edu>
2019-04-04 21:58:28 -0400

    i
    Added basic CSS that needs expanding for both global and page specific index css files.
    	Global will be used across all and must be included in every page
    Trying to create specific keys for each entry using the username as primary key
    	Unable to properly formulate child relationship in heirarchy structure
    	Continuing to update code for primary keys

--- public/index.js
+++ public/index.js
@@ -1,14 +1,18 @@
 //-------------------------------------------------------------------------------------
-function saveUser() {
-	firebase.database().ref("Users/").push({
-	//Get Elements
-		Username: document.getElementById('txtUsername').value,
-		//Email:	document.getElementById('txtEmail').value,
-		//Name:	document.getElementById('txtName').value
-})
-}
-
-
 function redirect() {
      window.location.assign("https://seniorsemgit.firebaseapp.com/kanban.html")
+}
+
+function saveUser() {
+	//firebase.database().ref("Users/").push({
+	//Get Elements
+	//Username: document.getElementById('txtUsername').value,
+	var dbRef = firebase.database();
+	var usersRef = dbRef.child("Users/");
+	usersRef.child(document.getElementById('txtUsername').value).set({
+		Username: document.getElementById('txtUsername').value
+}, onComplete)
 };
+
+
+;

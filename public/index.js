394a047b4076fac5a71f28b8dc42288b71d36843
isaiahf06 <isaiahf_0318@yahoo.com>
2019-04-07 16:41:56 -0400

    Took kanban CSS into its own page and out of the main html page. Added a line to index.js for the purpose of bringing over the username into the main kanban page to read of their unique data. Added lines in kanban.html to simply state "hello" username for whoever just logged in.

--- public/index.js
+++ public/index.js
@@ -7,6 +7,7 @@ function saveUser() {
 	//firebase.database().ref("Users/").push({
 	//Get Elements
 	//Username: document.getElementById('txtUsername').value,
+	sessionStorage.setItem('User', document.getElementById('txtUsername').value; //Bring user name into Kanban page
 	var dbRef = firebase.database().ref("Users/");
 	var usersRef = dbRef.child(document.getElementById('txtUsername').value).set({
 		Username: document.getElementById('txtUsername').value

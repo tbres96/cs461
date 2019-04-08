91bedb05e9de0364b0f121ad5ec8ad99956a0a87
Travis <Travis.Bresnahan@live.longwood.edu>
2019-04-07 20:07:39 -0400

    Reverted (Switching to local testing now)

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-
+//-------------------------------------------------------------------------------------
 function redirect() {
 	window.location.replace("https://seniorsemgit.firebaseapp.com/kanban.html")
 }
@@ -7,10 +7,10 @@ function saveUser() {
 	//firebase.database().ref("Users/").push({
 	//Get Elements
 	//Username: document.getElementById('txtUsername').value,
-	sessionStorage.setItem('User', document.getElementById('txtUsername').value); //Bring user name into Kanban page
-	var dbRef = firebase.database().ref("Users/");
-	var usersRef = dbRef.child(document.getElementById('txtUsername').value).set({
-	
-	})
-	setTimeout(redirect, 1000)
-};
+	sessionStorage.setItem('User', document.getElementById('txtUsername').value; //Bring user name into Kanban page
+		var dbRef = firebase.database().ref("Users/");
+		var usersRef = dbRef.child(document.getElementById('txtUsername').value).set({
+			Username: document.getElementById('txtUsername').value
+		})
+		setTimeout(redirect, 1000)
+	}

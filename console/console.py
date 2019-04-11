20b986b3103957921cd752580b649cbc3943544b
Anderw1234567 <adsmitty70@yahoo.com>
2019-04-10 21:01:13 -0400

    Fixed many issues and bool error

--- console/console.py
+++ console/console.py
@@ -18,15 +18,15 @@ print("Fetching users and boards...")
 
 users = db.child("Users").get()
 board = db.child("Boards").get()
-board1 = db.child("Boards").child("Board1").get()
-column1 = db.child("Boards").child("Board1").child("Tasks").get()
 
 #currentUserString = getpass.getuser()
 currentUserString = "Andrew Smith"
 currentUser = getCertainUser(users, currentUserString)
+nameString = "Stranger"
+if (currentUser != False):
+    nameString = currentUser.name
 
-
-print("Welcome %s to Super Seven KanBan! Enter 'quit' to exit." % currentUser.name)
+print("Welcome %s to Super Seven KanBan! Enter 'quit' to exit." % nameString)
 introBoardString = "You are currently a member of: "
 
 for s in currentUser.boardStringList:
@@ -49,23 +49,7 @@ while(1):
         break
 
     if (inputString == "users"):
-        #users = db.child("Users").get()
         getAllUsers(users)
-        
-##        for user in users.each():
-##            newUser = User()
-##            print("USER: ", user.key())
-##            newUser.name = user.key()
-##            for key, value in user.val().items():
-##                #newUser.change_attribute(key, value)
-##                if (value == 1):
-##                    newUser.boardList.append(key)
-##                print("key: ", key," Value: ", value)
-                
-            #print("NATIVE OUTPUT: ", user.key(),": ", user.val())
-
-    
-
 
     if (inputString == "boards"):
         getAllBoards(board)

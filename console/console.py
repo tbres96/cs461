b8f4a1a544c8bb9a2e1127fe3482da783a27e377
Anderw1234567 <adsmitty70@yahoo.com>
2019-04-15 17:38:10 -0400

    Fixed many bugs and added adding tasks.

--- console/console.py
+++ console/console.py
@@ -17,7 +17,9 @@ db = firebase.database()
 
 board = db.child("Boards").get()
 users = db.child("Users").get()
+currentBoard = -1
 
+#getAllBoards(board, 0)
 
 print("Fetching users and boards...")
 
@@ -29,11 +31,12 @@ nameString = "Stranger"
 if (currentUser != False):
     nameString = currentUser.name
 
-print("Welcome %s to Super Seven KanBan! Enter 'quit' to exit." % nameString)
+print('\n', '\n', "Welcome %s to Super Seven KanBan! Enter 'quit' to exit." % nameString)
 introBoardString = "You are currently a member of: "
 
 for s in currentUser.boardStringList:
     introBoardString += s + ", "
+    boardToAdd = Board()
     boardToAdd = getCertainBoard(board, s)
     if (boardToAdd):
         currentUser.boardObjectList.append(boardToAdd)
@@ -51,13 +54,37 @@ while(1):
         print("Goodbye!")
         break
 
+    if (inputString == "select"):
+        print("Which board would you like to select?")
+        boardToSelect = input("  > ")
+        boardFound = False
+        for  boardName in currentUser.boardStringList:
+            if (boardToSelect != boardName):
+                continue
+            elif (boardToSelect == boardName):              
+                board = db.child("Boards").get()
+                currentBoard = getCertainBoard(board, boardToSelect)
+                print("Found your board!")
+                boardFound = True
+                printBoard(currentBoard)
+                print('\n', '\n', "You are currently working on the ", currentBoard.name, " board. You can now <add task> and <edit task>.")
+                break
+
+        if (boardFound == False):
+            print("Could not find your board. Are you sure you're a part of it?")
+
+
+    if (inputString == "add task"):
+        addTask(currentBoard, currentUser)
+        
+
     if (inputString == "users"):
         localusers = db.child("Users").get()
         getAllUsers(localusers)
 
     if (inputString == "boards"):
         localboard = db.child("Boards").get()
-        getAllBoards(localboard)
+        getAllBoards(localboard, 1)
 
     if (inputString == "view boards"):
         for b in currentUser.boardObjectList:

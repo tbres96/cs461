cf62ea259c8076bbc5ae29aed4179da62bfc6b9b
isaiahf06 <isaiahf_0318@yahoo.com>
2019-04-16 16:25:26 -0400

    Adding database functionality to kanban
    Made every session determined on the user logged in so that they see
    their boards and task for each respective board. Made some changes to
    css.
    
    Known bugs - clicking buttons for adding and removing users or owners
    are not fully implemented therefore may cause errors if clicked on.
    Clicking on a task link does not lead to the task page at the moment, but is being
    worked on.

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -9,6 +9,219 @@ var OWNERS = "Owners";
 var LASTACTION = "LatestAction";
 var DUEDATE = "DueDate";
 
+
+
+function switchKanbanView(id, name){	
+	sessionStorage.setItem('boardName',name);
+	sessionStorage.setItem('boardID',id);
+}
+
+/*
+Variable Used for testing purposes
+var n = 0;
+*/
+
+function getListOfKanbans(){
+	var boardRef = db.ref().child(BOARDS);
+	boardRef.on('value', function(snapshot){
+		//if BOARDS exist
+		if(snapshot.exists()){
+			//if BOARDS has any boards
+			if(snapshot.hasChildren()){
+				//for every board that exist
+				var kanbanList = "<hr class='hr1'>";
+				snapshot.forEach(function(childSnapshot){
+					//set the name and ID of the board to a variable
+					var boardName = childSnapshot.child(NAME).val();
+					var boardID = childSnapshot.key;
+					//check if any users exist
+					if(childSnapshot.child(USERS).exists()){
+						//look through every non currently logged in user
+						var getUsers = "";
+						var getOtherUsers = "";
+						childSnapshot.child(USERS).forEach(function(userSnapshot){
+							//if their values are true
+							if(userSnapshot.val()){
+								//n++; used for testing purposes
+								if(userSnapshot.key != 'mellita'){ //sessionStorage.getItem('User') //'mellita') <<-- used for testing locally
+									//n++; used for testing purposes
+									getOtherUsers += userSnapshot.key + " ";
+								}
+							}
+						});
+						childSnapshot.child(USERS).forEach(function(userSnapshot){
+							if(userSnapshot.val()){
+								//if values match current user logged in
+								if(userSnapshot.key == 'mellita'){ //sessionStorage.getItem('User') //'mellita') <<-- used for testing locally
+									getUsers += userSnapshot.key + " ";
+									//create list of kanbans that are links
+									kanbanList += "<a href='' onclick='switchKanbanView(this.id, this.title)' id='" + boardID + "' title='" + boardName + "'>" + boardName + "</a>";
+									kanbanList += "<hr class='hr1'>";
+									//check the values of the current storage session for kanban board and id and based on thier value insert the following into variable
+									if((sessionStorage.getItem('boardName') == null) && (sessionStorage.getItem('boardID') == null)){
+										sessionStorage.setItem('boardName', boardName);
+										var displayBoardName = "<h1>Now Viewing - " + sessionStorage.getItem('boardName') + "</h1>";
+										sessionStorage.setItem('boardID', boardID);
+										var displayBoardID = "<h2>Board ID - " + sessionStorage.getItem('boardID') + "</h2>"; 
+									}
+									else{
+										var displayBoardName = "<h1>Now Viewing - " + sessionStorage.getItem('boardName') + "</h1>"; 
+										var displayBoardID = "<h2>Board ID - " + sessionStorage.getItem('boardID') + "</h2>";
+									}
+									//display boardname
+									document.getElementById("nameOfBoard").innerHTML = displayBoardName; 
+									//display boardID
+									document.getElementById("idOfBoard").innerHTML = displayBoardID;
+									//display kanbanList
+									document.getElementById("kanbans").innerHTML = kanbanList;
+
+									//before moving forward make sure the tasks we are looking at are under the current board session storage
+									if(sessionStorage.getItem('boardName') == boardName){
+										//check if any task columns exist
+										if(childSnapshot.child(TASKS).exists()){
+											//look through every task column
+											childSnapshot.child(TASKS).forEach(function(columnSnapshot){
+												if(columnSnapshot.key == 'Todo'){
+													//display column name
+													var columnName = columnSnapshot.key;
+													var displayColumnName = "<p style='font-size:25px;text-align:center;'><u>" + columnName + "</u></p>";
+													document.getElementById("nameOfColumn1").innerHTML = displayColumnName; 
+													//if column has task
+													if(columnSnapshot.hasChildren()){
+														//look through every task in this column and display their names
+														var taskDisplay = "<ul>";
+														columnSnapshot.forEach(function(taskSnapshot){
+															sessionStorage.setItem('column', columnName);
+															sessionStorage.setItem('task', taskSnapshot.child(NAME).val());
+															//taskDisplay += "<div class='box'><li><a href=''>TaskName: " + taskSnapshot.child(NAME).val();
+								taskDisplay += "<div class='box'><li><a href='javascript:setTask('Board1','Todo','Task1');'>TaskName: " + taskSnapshot.child(NAME).val();
+															//taskDisplay += "<div class='box'><li><a href='javascript:setTask(sessionStorage.getItem('boardName'), columnName, taskSnapshot.child(NAME).val());'>TaskName: " + taskSnapshot.child(NAME).val();
+															//taskDisplay += "<div class='box'><li><a href='setTask(sessionStorage.getItem('boardName'), columnName, taskSnapshot.child(NAME).val());'>TaskName: " + taskSnapshot.child(NAME).val();
+															//if owners has existing users
+															if(taskSnapshot.child(OWNERS).hasChildren()){
+																var ownerStart = "<br>Owners:";
+																var ownerName = " ";	
+																//Look at every user who owns task
+																taskSnapshot.child(OWNERS).forEach(function(ownerSnapshot){
+																	if(ownerSnapshot.val()){
+																		ownerName += ownerSnapshot.key + " ";
+																	}
+																});
+																//if every user is not an owner
+																if(!ownerName.replace(/\s/g, '').length){
+																	ownerStart = "";
+																}
+																taskDisplay += ownerStart + ownerName + "<br>Users: " + getUsers + " " + getOtherUsers + "</a>";
+																taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+																taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>";
+																taskDisplay += "</li></div><br>";
+															}
+															else{
+																taskDisplay += "<br>Users: " + getUsers + " " + getOtherUsers + "</a>";
+																taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+																taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>"; 
+																taskDisplay += "</li></div><br>";
+															}
+														});
+														taskDisplay += "</ul>";
+														document.getElementById("namesOfTodo").innerHTML = taskDisplay;
+													}
+												}
+												//repeat steps above for columns Doing and Done
+												if(columnSnapshot.key == 'Doing'){
+													var columnName = columnSnapshot.key;
+													var displayColumnName = "<p style='font-size:25px;text-align:center;'><u>" + columnName + "</u></p>";
+													document.getElementById("nameOfColumn2").innerHTML = displayColumnName;
+													if(columnSnapshot.hasChildren()){
+														var taskDisplay = "<ul>";
+														columnSnapshot.forEach(function(taskSnapshot){
+
+															sessionStorage.setItem('column', columnName);
+															sessionStorage.setItem('task', taskSnapshot.child(NAME).val());
+															taskDisplay += "<div class='box'><li><a href='' onclick='setTask(sessionStorage.getItem('boardName');, columnName, TaskSnapshot.child(NAME).val());'>TaskName: " + taskSnapshot.child(NAME).val();
+															if(taskSnapshot.child(OWNERS).hasChildren()){
+																var ownerStart = "<br>Owners:";
+																var ownerName = " ";
+																taskSnapshot.child(OWNERS).forEach(function(ownerSnapshot){
+																	if(ownerSnapshot.val()){
+																		ownerName += ownerSnapshot.key + " ";
+																	}
+																});
+																if(!ownerName.replace(/\s/g, '').length){
+																	ownerStart = "";
+																}
+																taskDisplay += ownerStart + ownerName + "<br>Users: " + getUsers + " " + getOtherUsers + "</a>";
+										taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+									taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>";
+																taskDisplay += "</li></div><br>";
+															}
+															else{
+																taskDisplay += "<br>Users: " + getUsers + " " + getOtherUsers + "</a>";
+																taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+																taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>";
+																taskDisplay += "</li></div><br>";
+															}
+														});
+														taskDisplay += "</ul>";
+														document.getElementById("namesOfDoing").innerHTML = taskDisplay;
+													}
+												}
+												if(columnSnapshot.key == 'Done'){
+													var columnName = columnSnapshot.key;
+													var displayColumnName = "<p style='font-size:25px;text-align:center;'><u>" + columnName + "</u></p>";
+													document.getElementById("nameOfColumn3").innerHTML = displayColumnName;
+													if(columnSnapshot.hasChildren()){
+														var taskDisplay = "<ul>";
+														columnSnapshot.forEach(function(taskSnapshot){
+															taskDisplay += "<div class='box'><li><a href='' onclick='setTask(sessionStorage.getItem('boardName');, columnName, TaskSnapshot.child(NAME).val());'>TaskName: " + taskSnapshot.child(NAME).val();
+															if(taskSnapshot.child(OWNERS).hasChildren()){
+																var ownerStart = "<br>Owners:";
+																var ownerName = " ";
+																taskSnapshot.child(OWNERS).forEach(function(ownerSnapshot){
+																	if(ownerSnapshot.val()){
+																		ownerName += ownerSnapshot.key + " ";
+																	}
+																});
+																if(!ownerName.replace(/\s/g, '').length){
+																	ownerStart = "";
+																}
+																taskDisplay += ownerStart + ownerName + "<br>Users: " + getUsers + " " + getOtherUsers + "</a>"; 																
+																taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+																taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>";
+																taskDisplay += "</li></div><br>";
+															}
+															else{
+																taskDisplay += "<br>Users: " + getUsers + " " + getOtherUsers + "</a>";
+																taskDisplay += "<button class='taskAddUsers' onclick='location.href=''; addUserToTask('mellita');'>Add an owner</button>";
+																taskDisplay += "<button class='taskRemoveUsers' onclick='location.href=''; removeUserFromTask('mellita');'>Remove an owner</button>";
+																taskDisplay += "</li></div><br>";
+
+															}
+														});
+														taskDisplay += "</ul>";
+														document.getElementById("namesOfDone").innerHTML = taskDisplay;
+													}
+												}
+
+											});
+
+										}
+
+
+									}
+								}
+							} 
+						}); 
+
+					}
+				}); 
+			}
+		}
+	});
+}
+
+
+
 function isLater(firstDate, secondDate){
 	if(firstDate > secondDate){
 		return 1;
